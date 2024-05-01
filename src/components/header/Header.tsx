530045c7ea1e725ba802fbfae4e9dad523873f9a
Danu Tryas <Danupristowo09@gmail.com>
2024-05-01 20:49:03 +0700

    add haeder

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,71 @@
-// "use client";
+"use client";
 
-import { isAuth } from "@/lib/isAuth";
-import HeaderClient from "./HeaderClient";
+import Image from "next/image";
+import SpotifyLoginButton from "../button/SpotifyLogin";
+import getSession from "@/lib/Session";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { WhiteLogo } from "../icons/Logo";
+import { signOut } from "next-auth/react";
 
-const Header = () => {
+interface Header {
+  isAuth: boolean;
+}
+
+const Header: React.FC<Header> = ({ isAuth }) => {
+  const session = getSession();
+  const pathname = usePathname();
+
+  const logouts = () => {
+    signOut();
+  };
+
   return (
     <div className="w-full h-full rounded-full border-2 border-white mt-5">
-      <div className="flex justify-between p-2 items-center px-2">
-        <div>Route</div>
-        <div>
-          <a href="/">Home</a>
+      <div className="flex justify-between p-2 items-center px-2 text-white">
+        <ul className="pl-5 flex gap-4 basis-2/5">
+          <li
+            className={`border-b-2 py-1 min-w-14 flex justify-center ${
+              pathname.startsWith("/track")
+                ? "border-white"
+                : "border-transparent"
+            }`}
+          >
+            <Link href="/track">TRACK</Link>
+          </li>
+          <li
+            className={`border-b-2  py-1 min-w-14 flex justify-center ${
+              pathname.startsWith("/profile")
+                ? "border-white"
+                : "border-transparent"
+            }`}
+          >
+            <Link href="/profile">PROFILE</Link>
+          </li>
+        </ul>
+        <div className="basis-1/5 flex justify-center">
+          <Link href="/">
+            <WhiteLogo />
+          </Link>
         </div>
-        <div className="flex justify-end">
-          <HeaderClient isAuth={isAuth()} />
+        <div className="flex justify-end basis-2/5 w-10 h-10 items-center gap-2">
+          {isAuth ? (
+            <>
+              <h2 onClick={logouts}>{session?.user.name}</h2>
+              <Image
+                alt="user-profile"
+                src={session?.user.picture}
+                width={40}
+                height={40}
+                style={{
+                  objectFit: "cover",
+                  borderRadius: "20px",
+                }}
+              />
+            </>
+          ) : (
+            <SpotifyLoginButton />
+          )}
         </div>
       </div>
     </div>
