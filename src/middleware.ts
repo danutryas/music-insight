1c4a2e3ddf93ff4d76b2c5c8c125b3829b666f1e
Danu Tryas <Danupristowo09@gmail.com>
2024-05-01 20:47:45 +0700

    remove bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,14 +9,14 @@ export default async function handler(req: NextRequest, res: NextApiResponse) {
     req.cookies.has("next-auth.session-token") ||
     req.cookies.has("__Secure-next-auth.session-token");
 
-  if (!cookie) {
-    return NextResponse.redirect(new URL("/auth/login", req.url));
-  } else {
-    if (req.nextUrl.pathname.match("/auth/login")) {
-      return NextResponse.redirect(new URL("/", req.url));
-    }
-    return NextResponse.next();
-  }
+  // if (!cookie) {
+  //   return NextResponse.redirect(new URL("/auth/login", req.url));
+  // } else {
+  //   if (req.nextUrl.pathname.match("/auth/login")) {
+  //     return NextResponse.redirect(new URL("/", req.url));
+  //   }
+  //   return NextResponse.next();
+  // }
 }
 export const config = {
   matcher: ["/((?!api|_next|.*\\..*).*)"],
