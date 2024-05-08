f32052a9d1809f9cf8b9d9fbab009dfda4125829
Danu Tryas <danu.tryas@bankneo.co.id>
2024-05-08 17:50:23 +0700

    add another services api call

--- src/services/player.ts
+++ src/services/player.ts
@@ -9,3 +9,24 @@ export const getPlaybackState = async () => {
   const response = await AxiosSpotify.get("/me/player");
   return response.data;
 };
+
+export const skipToNext = async () => {
+  await AxiosSpotify.post("/me/player/next");
+};
+
+export const skipToPrevious = async () => {
+  await AxiosSpotify.post("/me/player/previous");
+};
+
+export const pausePlayback = async () => {
+  await AxiosSpotify.put("/me/player/pause");
+};
+export const resumePlayback = async () => {
+  await AxiosSpotify.put("/me/player/play");
+};
+
+export const updatePlaybackVolume = async (volume: number) => {
+  await AxiosSpotify.put(
+    "https://api.spotify.com/v1/me/player/volume?volume_percent=" + volume
+  );
+};
